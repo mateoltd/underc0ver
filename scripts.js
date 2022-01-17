019d8e833c3ce8d4f87b3b12b4e44025bb187cdf
Mathéo <96652538+soyzer0@users.noreply.github.com>
2022-01-17 13:19:52 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function uwebsite() {
+    document['write']('<style>*{margin:0;border:0;}</style><iframe src=\'https://bing.com\' height=\'100%\' width=\'100%\'></iframe>');
+
+}
+
+function uvideo() {
+    var thevalue = document.getElementById("thevideo").value;
+    document['write']('<style>*{margin:0;border:0;}</style><iframe src=\'https://ytunblock.glitch.me/downloadvid?v='+thevalue+'\' height=\'100%\' width=\'100%\'></iframe>');
+
+}
+
+function usweb() {
+    var thevalue = document.getElementById("thesite").value;
+    document['write']('<style>*{margin:0;border:0;}</style><iframe src=\https://holyubofficial.net?d#\ \' height=\'100%\' width=\'100%\'></iframe>');
+
+}
+
+function discordUB() {
+        var ifrm = document.createElement("iframe");
+        ifrm.setAttribute("src", "https://holyubofficial.net/?s");
+        document['write']('<style>*{margin:0;border:0;}</style><iframe src=\'https://holyubofficial.net?d#\' height=\'100%\' width=\'100%\'></iframe>');
+    }
+
+
+
